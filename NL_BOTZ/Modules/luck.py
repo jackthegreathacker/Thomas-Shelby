80f3532c64415832a3af472b0bf7848d2dd0fdd4
jackthegreathacker <109450895+jackthegreathacker@users.noreply.github.com>
2022-08-08 19:52:49 +0530

    Update luck.py

--- NL_BOTZ/Modules/luck.py
+++ NL_BOTZ/Modules/luck.py
@@ -1,4 +1,4 @@
-# code added by @lallu_tg
+# code added by @Jack_Network
 # use with proper credits
 
 
@@ -6,7 +6,7 @@ from pyrogram import Client, filters
 from info import COMMAND_HAND_LER
 from NL_BOTZ.helper_functions.cust_p_filters import f_onw_fliter
 
-# LUCK------------ https://telegram.me/Josprojects ------------ #
+# LUCK------------ https://telegram.me/Jack_Network ------------ #
 
 # EMOJI CONSTANTS
 TRY_YOUR_LUCK = "🎰"
@@ -16,7 +16,7 @@ TRY_YOUR_LUCK = "🎰"
     filters.command(["luck", "cownd"])
 )
 async def luck_cownd(client, message):
-    """ /luck an @animatedluck """
+    """ /luck an @Jack_Network """
     rep_mesg_id = message.message_id
     if message.reply_to_message:
         rep_mesg_id = message.reply_to_message.message_id
