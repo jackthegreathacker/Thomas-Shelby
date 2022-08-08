079b0814106dd951aa6373ca45a6f4133a29737f
jackthegreathacker <109450895+jackthegreathacker@users.noreply.github.com>
2022-08-08 19:49:58 +0530

    Update dice.py

--- NL_BOTZ/Modules/dice.py
+++ NL_BOTZ/Modules/dice.py
@@ -1,4 +1,4 @@
-# codes added by @lallu_tg
+# codes added by @Jack_Network
 # use with proper credits
 
 from pyrogram import Client, filters
@@ -15,7 +15,7 @@ DICE_E_MOJI = "🎲"
     f_onw_fliter
 )
 async def roll_dice(client, message):
-    """ @RollaDie """
+    """ @Jack_Network """
     rep_mesg_id = message.message_id
     if message.reply_to_message:
         rep_mesg_id = message.reply_to_message.message_id
